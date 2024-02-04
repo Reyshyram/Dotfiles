006f28f682669f92a8c480715f7f6fa27f69c46f
Reyshyram <thibault.gadenne@gmail.com>
2024-02-04 18:49:32 +0100

    Finished the config

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Make dnf better
+sudo cp config/dnf.conf /etc/dnf/dnf.conf
+
 # Install required packages
 sudo dnf copr enable peterwu/rendezvous
-sudo dnf install git firefox pcmanfm gnome-tweaks bibata-cursor-themes gnome-themes-extra ark qt5ct hydrapaper
+sudo dnf install git firefox nemo gnome-tweaks bibata-cursor-themes gnome-themes-extra ark qt5ct hydrapaper neofetch vlc zsh kitty
+flatpak install flathub io.bassi.Amberol
 
 # Setting up gnome settings
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic' # Cursor theme
@@ -22,17 +26,23 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'firefox'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>f'
 
-# Pcmanfm
+# Nemo
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'File Browser'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'pcmanfm'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'nemo'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>e'
 
-# Kitty
+# Terminal
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name 'Terminal'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command 'kitty'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding '<Super>t'
+
+# Text editor
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ name 'Text Editor'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ command 'gnome-text-editor'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ binding '<Super>b'
 
 # Grub theme
 git clone https://github.com/catppuccin/grub.git
@@ -59,11 +69,34 @@ flatpak install flathub org.gnome.Extensions
 rm ~/.themes/Catppuccin-Mocha-Standard-Lavender-Dark/gnome-shell/gnome-shell.css
 cp config/gnome-shell.css ~/.themes/Catppuccin-Mocha-Standard-Lavender-Dark/gnome-shell/gnome-shell.css
 
-# Gnome terminal theme
-curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v0.2.0/install.py | python3 -
+# Set nemo as default
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 
+# Kitty as default
+sudo mv /usr/bin/gnome-terminal /usr/bin/gnome-terminal.NOPE
+sudo ln -sfv /usr/bin/kitty /usr/bin/gnome-terminal
 
-xdg-mime default pcmanfm.desktop inode/directory application/x-gnome-saved-search
+# Zsh config
+chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/themes
+git clone https://github.com/catppuccin/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/themes/catppuccin
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf
+wget https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf
+mv MesloLGS* /home/reyshyram/.local/share/fonts
+cp config/.zshrc ~/.zshrc
+cp config/.p10k.zsh ~/.p10k.zsh
+
+# Kitty config
+mkdir -p ~/.config/kitty
+cp -r config/kitty/* ~/.config/kitty
 
 echo "Please launch the extensions app and add User Themes, then add Catppuccinn as the shell theme"
-echo "Please launch the Gnome Terminal and enable Catppuccinn as the theme"
+echo "Please install extensions and configure them"
+echo "Please set default apps"
