f8e7bf6d2bf4b6c5a8621655cf01f743f1923f3b
Reyshyram <102749075+Reyshyram@users.noreply.github.com>
2024-02-14 20:39:23 +0100

    Added Github Desktop

--- install.sh
+++ install.sh
@@ -14,8 +14,12 @@ flatpak install flathub one.ablaze.floorp
 # Install Visual Studio Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
 sudo dnf install code
+
+# Install Github Desktop
+sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
+sudo dnf install github-desktop
 
 # Setting up gnome settings
 gsettings set org.gnome.desktop.interface icon-theme 'MoreWaita' # Icon Theme
