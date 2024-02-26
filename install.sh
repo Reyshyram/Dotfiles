4c7b7d1810803dacdc09fb0f918dde2e40ff7b10
Reyshyram <thibault.gadenne@gmail.com>
2024-02-26 13:31:01 +0100

    Btop

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ cd yay
 makepkg -si
 cd ..
 rm -rf yay
-sudo pacman -S micro wl-clipboard os-prober kitty hyprland qt5-graphicaleffects qt5-quickcontrols2 qt5-svg noto-fonts neofetch plymouth ttf-firacode-nerd zsh qt5-wayland qt6-wayland plymouth pipewire wireplumber xdg-desktop-portal-hyprland xdg-desktop-portal-gtk pacman-contrib pcmanfm-qt gnome-keyring
+sudo pacman -S micro wl-clipboard os-prober kitty hyprland qt5-graphicaleffects qt5-quickcontrols2 qt5-svg noto-fonts neofetch plymouth ttf-firacode-nerd zsh qt5-wayland qt6-wayland plymouth pipewire wireplumber xdg-desktop-portal-hyprland xdg-desktop-portal-gtk pacman-contrib pcmanfm-qt gnome-keyring btop
 yay -S sddm-git bibata-cursor-theme ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin floorp-bin amberol
 
 # SDDM Configuration
@@ -82,6 +82,10 @@ sudo plymouth-set-default-theme -R catppuccin-mocha
 # Disable mouse acceleration
 sudo sed -i '/Identifier "libinput pointer catchall"/,/^EndSection/{/^EndSection/i \        Option "Accel Profile Enabled" "0 1 0"
 }' /usr/share/X11/xorg.conf.d/40-libinput.conf
+
+# Btop theme
+mkdir -p ~/.config/btop/themes/
+cp -r ./config/btop/* ~/.config/btop/
 
 # Copy Hyprland config
 mdir -p ~/.config/hypr
