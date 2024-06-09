caad457639b43bfeeb8eb055ae4b431e2af07310
Reyshyram <thibault.gadenne@gmail.com>
2024-06-09 12:18:32 +0200

    Grub theme

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ cd yay
 makepkg -si
 cd ..
 rm -rf yay
-sudo pacman -S --needed --noconfirm micro wl-clipboard os-prober kitty hyprland qt5-graphicaleffects qt5-quickcontrols2 qt5-svg noto-fonts neofetch plymouth ttf-firacode-nerd zsh qt5-wayland qt6-wayland plymouth pipewire wireplumber xdg-desktop-portal-hyprland pacman-contrib nemo btop nwg-look qt5ct qt6ct papirus-icon-theme kvantum sddm brightnessctl pamixer playerctl xdg-user-dirs sound-theme-freedesktop yad jq file-roller vlc shotwell tumbler ffmpegthumbnailer polkit-gnome udiskie grim socat pipewire wireplumber networkmanager pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse gst-plugin-pipewire cliphist slurp swappy noto-fonts-emoji firewalld waybar xdg-desktop-portal-gtk bluez bluez-utils blueman network-manager-applet pavucontrol ttf-meslo-nerd gnome-keyring ntfs-3g kooha nemo-fileroller kvantum-qt5 gnome-disk-utility firefox
-yay -S --needed --noconfirm bibata-cursor-theme ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin g4music swaync hardcode-fixer-git papirus-folders-catppuccin-git catppuccin-gtk-theme-mocha rofi-lbonn-wayland nwg-drawer-bin wlogout xwaylandvideobridge github-desktop-bin hyprlock swww hypridle hyprpicker grimblast-git aurutils arch-update nwg-displays wlr-randr
+sudo pacman -S --needed --noconfirm micro wl-clipboard os-prober kitty hyprland qt5-graphicaleffects qt5-quickcontrols2 qt5-svg noto-fonts neofetch plymouth ttf-firacode-nerd zsh qt5-wayland qt6-wayland plymouth pipewire wireplumber xdg-desktop-portal-hyprland pacman-contrib nemo btop nwg-look qt5ct qt6ct papirus-icon-theme kvantum sddm brightnessctl pamixer playerctl xdg-user-dirs sound-theme-freedesktop yad jq file-roller vlc shotwell tumbler ffmpegthumbnailer polkit-gnome udiskie grim socat pipewire wireplumber networkmanager pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse gst-plugin-pipewire cliphist slurp swappy noto-fonts-emoji firewalld waybar xdg-desktop-portal-gtk bluez bluez-utils blueman network-manager-applet pavucontrol ttf-meslo-nerd gnome-keyring kooha nemo-fileroller kvantum-qt5 gnome-disk-utility firefox
+yay -S --needed --noconfirm bibata-cursor-theme ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin g4music swaync hardcode-fixer-git rofi-lbonn-wayland nwg-drawer-bin wlogout xwaylandvideobridge github-desktop-bin hyprlock swww hypridle hyprpicker grimblast-git aurutils arch-update nwg-displays wlr-randr
 
 # SDDM Configuration
 echo "Preparing SDDM theme..."
@@ -29,12 +29,12 @@ sudo chown $USER /usr/share/sddm/themes/sugar-candy/Backgrounds/cache.png
 
 # Grub theme and configuration
 echo "Preparing grub theme..."
-git clone https://github.com/catppuccin/grub.git
+git clone https://github.com/Coopydood/HyperFluent-GRUB-Theme.git
 sudo mkdir -p /usr/share/grub/themes/
-sudo cp -r ./grub/src/* /usr/share/grub/themes/
-rm -rf ./grub
+sudo cp -r ./HyperFluent-GRUB-Theme/arch /usr/share/grub/themes/
+rm -rf ./HyperFluent-GRUB-Theme
 sudo sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
-sudo sed -i 's|^#GRUB_THEME="/path/to/gfxtheme"|GRUB_THEME="/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt"|' /etc/default/grub
+sudo sed -i 's|^#GRUB_THEME="/path/to/gfxtheme"|GRUB_THEME="/usr/share/grub/themes/arch/theme.txt"|' /etc/default/grub
 sudo sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/; s/^#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
