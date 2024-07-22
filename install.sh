479aa5e6f4640ae0e3102f1569dc72448cea8dfa
Reyshyram <thibault.gadenne@gmail.com>
2024-07-22 23:00:21 +0200

    some apps

--- install.sh
+++ install.sh
@@ -104,13 +104,12 @@ PACKAGES=(
     libreoffice-fresh evince gnome-clocks p7zip unrar swww imagemagick
     gstreamer gst-plugins-bad gst-plugins-base gst-plugins-good
     gst-plugins-ugly pkgconf pinta vim fzf reflector zoxide wget
-    zenity
+    zenity baobab gnome-font-viewer
 )
 
 # AUR packages to install
 YAY_PACKAGES=(
-    bibata-cursor-theme ttf-meslo-nerd-font-powerlevel10k
-    visual-studio-code-bin g4music hardcode-fixer-git
+    bibata-cursor-theme visual-studio-code-bin g4music hardcode-fixer-git
     nwg-drawer-bin wlogout xwaylandvideobridge github-desktop-bin
     hyprpicker grimblast-git aurutils arch-update nwg-displays
     wlr-randr python-zombie-imp gradience adw-gtk-theme pywal-16-colors
