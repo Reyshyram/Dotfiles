130ba2e0fed625524fc1a7d01c2c78d18ff07a42
Reyshyram <thibault.gadenne@gmail.com>
2024-07-29 12:33:54 +0200

    Better script

--- config/hypr/scripts/volume.sh
+++ config/hypr/scripts/volume.sh
@@ -2,42 +2,7 @@
 
 # Notify
 notify_user() {
-    theme="freedesktop" # Set the theme for the system sounds.
-
-    # Set the directory defaults for system sounds.
-    userDIR="$HOME/.local/share/sounds"
-    systemDIR="/usr/share/sounds"
-    defaultTheme="freedesktop"
-
-    # Prefer the user's theme, but use the system's if it doesn't exist.
-    sDIR="$systemDIR/$defaultTheme"
-    if [ -d "$userDIR/$theme" ]; then
-        sDIR="$userDIR/$theme"
-    elif [ -d "$systemDIR/$theme" ]; then
-        sDIR="$systemDIR/$theme"
-    fi
-
-    # Get the theme that it inherits.
-    iTheme=$(grep -i "inherits" "$sDIR/index.theme" | cut -d "=" -f 2)
-    iDIR="$sDIR/../$iTheme"
-
-    # Find the volume change sound file and play it.
-    sound_file=$(find $sDIR/stereo -name "audio-volume-change.*" -print -quit)
-    if ! test -f "$sound_file"; then
-        sound_file=$(find $iDIR/stereo -name "audio-volume-change.*" -print -quit)
-        if ! test -f "$sound_file"; then
-            sound_file=$(find $userDIR/$defaultTheme/stereo -name "audio-volume-change.*" -print -quit)
-            if ! test -f "$sound_file"; then
-                sound_file=$(find $systemDIR/$defaultTheme/stereo -name "audio-volume-change.*" -print -quit)
-                if ! test -f "$sound_file"; then
-                    echo "Error: Sound file not found."
-                    exit 1
-                fi
-            fi
-        fi
-    fi
-
-    pw-play "$sound_file"
+    pw-play "$HOME/.config/swaync/sounds/audio-volume-change.oga"
 }
 
 # Increase Volume
